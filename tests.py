f14cbb1923c10fc7ce839d870d47632a8ce7fb66
TheOnlyWayUp <76237496+TheOnlyWayUp@users.noreply.github.com>
2021-12-03 19:59:33 +0530

    Update tests.py

--- tests.py
+++ tests.py
@@ -1,17 +1,19 @@
-#NEEDS `pip install cprint` TO FUNCTION
+#NEEDS PIP INSTALL CPRINT TO WORK
 
-import mcstalker, asyncio
+import MCStalker, asyncio
 from cprint import *
 
 allGood = 0
 
 
 def testApiKeys():
+    """Test if the API is working correctly.
+    """
     global allGood
     cprint("------------ CURRENTLY TESTING BAD API KEYS ------------\n")
-    stats = functions.Stats("balls")
-    server = functions.Server("balls")
-    player = functions.Player("balls")
+    stats = MCStalker.Stats("balls")
+    server = MCStalker.Server("balls")
+    player = MCStalker.Player("balls")
 
     asyncio.run(stats.returnStats())
     cprint.ok("STATS CHECK SUCCESSFUL (1/4)")
@@ -19,19 +21,19 @@ def testApiKeys():
 
     try:
         asyncio.run(server.returnServer("46.228.199.189"))
-    except functions.MCStalker.invalidApiKey:
+    except MCStalker.MCStalker.invalidApiKey:
         allGood += 1
         cprint.ok("SERVER CHECK SUCCESSFUL (2/4)")
 
     try:
         asyncio.run(server.returnTopServers())
-    except functions.MCStalker.invalidApiKey:
+    except MCStalker.MCStalker.invalidApiKey:
         allGood += 1
         cprint.ok("TOP SERVERS CHECK SUCCESSFUL (3/4)")
 
     try:
         asyncio.run(player.returnPlayer("TheOnlyWayUp"))
-    except functions.MCStalker.invalidApiKey:
+    except MCStalker.MCStalker.invalidApiKey:
         allGood += 1
         cprint.ok("PLAYER CHECK SUCCESSFUL (4/4)")
 
@@ -39,13 +41,15 @@ def testApiKeys():
 
 
 def testSearches():
+    """Test if the API is working correctly.
+    """
     global allGood
     cprint("\n\n------------ CURRENTLY TESTING SEARCHES ------------\n")
     key = input("Enter your API Key: ")
 
-    stats = functions.Stats(key)
-    server = functions.Server(key)
-    player = functions.Player(key)
+    stats = MCStalker.Stats(key)
+    server = MCStalker.Server(key)
+    player = MCStalker.Player(key)
 
     try:
         asyncio.run(stats.returnStats())
